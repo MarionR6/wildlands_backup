7dcf4bd84a64619464e45d242d600a660f3e2dfd
MarionR6 <marionrenuy@gmail.com>
2023-07-26 12:33:23 +0200

    :sparkles: new pop up wip:

--- js/script.js
+++ js/script.js
@@ -12,3 +12,42 @@ toggle.onclick = function(){
     toggle.classList.toggle('active')
     navigation.classList.toggle('active')
 }
+
+// POP UP CONNEXION
+
+const connexion = document.getElementById ('email');
+const password = document.getElementById ('password');
+
+// FETCH
+
+const dataUser = "json/data.json"
+
+const fetchUser = async () => {
+    const response = await fetch (dataUser, {
+        method:"GET",
+        headers:{
+            "Accept" : "application/json"
+        },
+    })
+    .then(response => response.json())
+    .then (data => {
+        connexion.placeholder = `${data.users[0].email}`;
+        password.placeholder = `${data.users[0].password}`;
+    })
+}
+
+fetchUser()
+
+const checkbox = document.getElementById('checkbox');
+const emaildisable = document.getElementById("emaildisable");
+const passworddisable = document.getElementById("passworddisable");
+
+checkbox.addEventListener("change", () =>{
+    if(checkbox.checked){
+        emaildisable.classList.add("emaildisable");
+        passworddisable.classList.add("passworddisable");
+    }else{
+        emaildisable.classList.remove("emaildisable");
+        passworddisable.classList.remove("passworddisable");
+    }
+})
