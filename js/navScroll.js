bf6c05d2970a82de8d7629cfe2ad565f2676fa8f
Loic <lolo62.grand@gmail.com>
2023-07-27 23:09:52 +0200

    modification de la nav bar

--- js/navScroll.js
+++ js/navScroll.js
@@ -2,17 +2,22 @@
 
 const navScroll = document.querySelector('.navBar');
 let lastScroll = window.scrollY;
+let scrollBar = document.getElementById('scrollBar');
 
 window.addEventListener("scroll", () =>{
     if(lastScroll < window.scrollY){
-        navScroll.classList.remove("scrollDown");
-        navScroll.classList.remove("scrollAppear");
-        
+        //navScroll.classList.remove("scrollDown");
+        // navScroll.classList.remove("scrollAppear");
+        //navScroll.classList.add("scrollDesappear");
+        scrollBar.style.top = "-100px";
+        scrollBar.style.transition = "all 0.5s";
     }
     else{
-        navScroll.classList.add("scrollDown");
-        navScroll.classList.add("scrollAppear");
-        
+        // navScroll.classList.add("scrollDown");
+        // navScroll.classList.add("scrollAppear");
+        scrollBar.style.top = "0px";
+        scrollBar.style.transition = "all 0.5s";
+        console.log("non");
     }
     lastScroll = window.scrollY;
 })
