959f637e31cf71661059b96926a5ab04a058d0d1
AnthonyB0511 <137073922+AnthonyB0511@users.noreply.github.com>
2023-07-30 09:24:44 +0200

    :sparkles: :feat avancée sur la billetterie

--- js/montantPanier.js
+++ js/montantPanier.js
@@ -1,22 +1,5 @@
-// NBR DE NUITS
 
-const nuitmoins = document.getElementById("nuitMoins");
-const nuitplus = document.getElementById("nuitPlus");
-const txtNights = document.getElementById("number");
-console.log(nuitplus)
-let night = 0;
-txtNights.innerText=night;
-nuitplus.addEventListener("click",()=>{
-    night++;
-    txtNights.innerText=night;
-})
-nuitmoins.addEventListener("click",()=>{
-    if(night>0){
-        night--;
-        txtNights.innerText=night; 
-    }
-    
-})
+
 // LES DIFFERENTS PRIX
 const prixParcAttractionAdulte = 50;
 const prixParcAttractionEnfant = 30;
@@ -26,13 +9,19 @@ const prixDeuxParcsAdulte = 100;
 const prixDeuxParcsEnfant = 70;
 const chambre = 75;
 const aventure=25;
-const chalet=250;
-const zero = 0;
+const prixChalet=250;
 let price = 0;
 // poiur déterminer le prix final
 const finalPrice=document.getElementById("price");
 finalPrice.innerText=price;
-//pour remettre à zero cet élément précis
+//Fonction pour cacher le bouton moins
+const cacher=(nombre,bouton)=>{
+    if(nombre===0){
+        bouton.classList.add("nonvisible")
+    }else if (nombre===1){
+        bouton.classList.replace("nonvisible","visible")
+    }
+}
 
 // prix parc attraction
 //version adulte
@@ -40,19 +29,21 @@ const attractionAdulteMoins = document.getElementById("attractionAdulteMoins");
 const attractionAdulte = document.getElementById("attractionAdulte");
 const attractionAdultePlus = document.getElementById("attractionAdultePlus");
 let numberplaceAttractionAdulte = 0;
+cacher(numberplaceAttractionAdulte,attractionAdulteMoins)
 attractionAdulte.innerText=numberplaceAttractionAdulte;
 attractionAdultePlus.addEventListener("click",()=>{
     numberplaceAttractionAdulte++;
     attractionAdulte.innerText=numberplaceAttractionAdulte;
     price+=prixParcAttractionAdulte
     finalPrice.innerText=price;
+    cacher(numberplaceAttractionAdulte,attractionAdulteMoins)
 })
 attractionAdulteMoins.addEventListener("click",()=>{
     if(numberplaceAttractionAdulte>0){
         numberplaceAttractionAdulte--;
         attractionAdulte.innerText=numberplaceAttractionAdulte;
         price-=prixParcAttractionAdulte;
-        
+        cacher(numberplaceAttractionAdulte,attractionAdulteMoins)
         finalPrice.innerText=price;
     }
     
@@ -63,12 +54,14 @@ const attractionEnfantMoins = document.getElementById("attractionEnfantMoins");
 const attractionEnfant = document.getElementById("attractionEnfant");
 const attractionEnfantPlus = document.getElementById("attractionEnfantPlus");
 let numberplaceAttractionEnfant = 0;
+cacher(numberplaceAttractionEnfant,attractionEnfantMoins)
 attractionEnfant.innerText=numberplaceAttractionEnfant;
 attractionEnfantPlus.addEventListener("click",()=>{
     numberplaceAttractionEnfant++;
     attractionEnfant.innerText=numberplaceAttractionEnfant;
     price+=prixParcAttractionEnfant;
     finalPrice.innerText=price;
+    cacher(numberplaceAttractionEnfant,attractionEnfantMoins)
 })
 attractionEnfantMoins.addEventListener("click",()=>{
     if(numberplaceAttractionEnfant>0){
@@ -76,6 +69,7 @@ attractionEnfantMoins.addEventListener("click",()=>{
         attractionEnfant.innerText=numberplaceAttractionEnfant;
         price-=prixParcAttractionEnfant;
         finalPrice.innerText=price;
+        cacher(numberplaceAttractionEnfant,attractionEnfantMoins)
     }
     
 })
@@ -86,19 +80,21 @@ const natureAdulteMoins = document.getElementById("natureAdulteMoins");
 const natureAdulte = document.getElementById("natureAdulte");
 const natureAdultePlus = document.getElementById("natureAdultePlus");
 let numberplaceNatureAdulte = 0;
+cacher(numberplaceNatureAdulte,natureAdulteMoins)
 natureAdulte.innerText=numberplaceNatureAdulte;
 natureAdultePlus.addEventListener("click",()=>{
     numberplaceNatureAdulte++;
     natureAdulte.innerText=numberplaceNatureAdulte;
     price+=prixParcNaturelAdulte
     finalPrice.innerText=price;
+    cacher(numberplaceNatureAdulte,natureAdulteMoins)
 })
 natureAdulteMoins.addEventListener("click",()=>{
     if(numberplaceNatureAdulte>0){
         numberplaceNatureAdulte--;
         natureAdulte.innerText=numberplaceNatureAdulte;
         price-=prixParcNaturelAdulte;
-        
+        cacher(numberplaceNatureAdulte,natureAdulteMoins)
         finalPrice.innerText=price;
     }
     
@@ -108,12 +104,14 @@ const natureEnfantMoins = document.getElementById("natureEnfantMoins");
 const natureEnfant = document.getElementById("natureEnfant");
 const natureEnfantPlus = document.getElementById("natureEnfantPlus");
 let numberplaceNatureEnfant = 0;
+cacher(numberplaceNatureEnfant,natureEnfantMoins)
 natureEnfant.innerText=numberplaceNatureEnfant;
 natureEnfantPlus.addEventListener("click",()=>{
     numberplaceNatureEnfant++;
     natureEnfant.innerText=numberplaceNatureEnfant;
     price+=prixParcNaturelEnfant
     finalPrice.innerText=price;
+    cacher(numberplaceNatureEnfant,natureEnfantMoins)
 })
 natureEnfantMoins.addEventListener("click",()=>{
     if(numberplaceNatureEnfant>0){
@@ -121,6 +119,7 @@ natureEnfantMoins.addEventListener("click",()=>{
         natureEnfant.innerText=numberplaceNatureEnfant;
         price-=prixParcNaturelEnfant;
         finalPrice.innerText=price;
+        cacher(numberplaceNatureEnfant,natureEnfantMoins)
     }
     
 })
@@ -131,41 +130,216 @@ const twoParcsAdulteMoins = document.getElementById("twoParcsAdulteMoins");
 const twoParcsAdulte = document.getElementById("twoParcsAdulte");
 const twoParcsAdultePlus = document.getElementById("twoParcsAdultePlus");
 let numberplaceTwoParcsAdulte = 0;
+cacher(numberplaceTwoParcsAdulte,twoParcsAdulteMoins)
 twoParcsAdulte.innerText=numberplaceTwoParcsAdulte;
 twoParcsAdultePlus.addEventListener("click",()=>{
     numberplaceTwoParcsAdulte++;
     twoParcsAdulte.innerText=numberplaceTwoParcsAdulte;
     price+=prixDeuxParcsAdulte
     finalPrice.innerText=price;
+    cacher(numberplaceTwoParcsAdulte,twoParcsAdulteMoins)
 })
 twoParcsAdulteMoins.addEventListener("click",()=>{
     if(numberplaceTwoParcsAdulte>0){
         numberplaceTwoParcsAdulte--;
         twoParcsAdulte.innerText=numberplaceNatureAdulte;
         price-=prixDeuxParcsAdulte;
-        
+        cacher(numberplaceTwoParcsAdulte,twoParcsAdulteMoins)
         finalPrice.innerText=price;
     }
     
 })
 // enfant
-const twoParcsEnfantMoins = document.getElementById("twoParcsEnfantsMoins");
+const twoParcsEnfantsMoins = document.getElementById("twoParcsEnfantsMoins");
 const twoParcsEnfant = document.getElementById("twoParcsEnfants");
-const twoParcsEnfantPlus = document.getElementById("twoParcsEnfantsPlus");
+const twoParcsEnfantsPlus = document.getElementById("twoParcsEnfantsPlus");
 let numberplaceTwoParcsEnfant = 0;
+cacher(numberplaceTwoParcsEnfant,twoParcsEnfantsMoins)
 twoParcsEnfant.innerText=numberplaceTwoParcsEnfant;
-twoParcsEnfantPlus.addEventListener("click",()=>{
+twoParcsEnfantsPlus.addEventListener("click",()=>{
     numberplaceTwoParcsEnfant++;
-    twoParcsEnfants.innerText=numberplaceTwoParcsAdulte;
+    twoParcsEnfant.innerText=numberplaceTwoParcsEnfant;
     price+=prixDeuxParcsEnfant
     finalPrice.innerText=price;
+    cacher(numberplaceTwoParcsEnfant,twoParcsEnfantsMoins)
 })
-twoParcsEnfantMoins.addEventListener("click",()=>{
+twoParcsEnfantsMoins.addEventListener("click",()=>{
     if(numberplaceTwoParcsEnfant>0){
         numberplaceTwoParcsEnfant--;
         twoParcsEnfant.innerText=numberplaceTwoParcsEnfant;
         price-=prixDeuxParcsEnfant;
         finalPrice.innerText=price;
+        cacher(numberplaceTwoParcsEnfant,twoParcsEnfantsMoins)
     }
     
 })
+
+const priceNight=()=>{
+    // NBR DE NUITS
+    const nuitmoins = document.getElementById("nuitMoins");
+    const nuitplus = document.getElementById("nuitPlus");
+    const txtNights = document.getElementById("number");
+    let night = 0;
+    let nbHotel=0;
+    let nbHotelEau=0;
+    let nbHamacs=0;
+    let nbChalet = 0;
+    let nbCamping=0;
+    let nbBulles=0;
+    txtNights.innerText=night;
+    finalPrice.innerText=price
+    cacher(night,nuitmoins)
+    nuitplus.addEventListener("click",()=>{
+        night++;
+        txtNights.innerText=night;
+        cacher(night,nuitmoins)
+        price+=(nbHotel*chambre)+(nbHotelEau*chambre)+(nbHamacs*aventure)+(nbChalet*prixChalet)+(nbCamping*aventure)+(nbBulles*chambre)
+        finalPrice.innerText=price;
+
+        
+    })
+    nuitmoins.addEventListener("click",()=>{
+            night--;
+            txtNights.innerText=night;
+            cacher(night,nuitmoins)
+            price-=(nbHotel*chambre)+(nbHotelEau*chambre)+(nbHamacs*aventure)+(nbChalet*prixChalet)+(nbCamping*aventure)+(nbBulles*chambre)
+            finalPrice.innerText=price;
+            
+            
+    })
+    //RESERVATION LOGEMENT
+
+
+    const hotel = document.getElementById("hotel");
+    const hotelMoins = document.getElementById("hotelMoins");
+    const hotelPlus = document.getElementById("hotelPlus");
+    hotel.innerText=nbHotel;
+    cacher(nbHotel,hotelMoins)
+    hotelPlus.addEventListener("click",()=>{
+        nbHotel++;
+        hotel.innerText=nbHotel;
+        price+=(chambre*night);
+        finalPrice.innerText=price;
+        cacher(nbHotel,hotelMoins)
+    })
+    hotelMoins.addEventListener("click",()=>{
+        if(nbHotel>0){
+            nbHotel--;
+            hotel.innerText=nbHotel;
+            price-=(chambre*night);
+            finalPrice.innerText=price;
+            cacher(nbHotel,hotelMoins)
+        }
+    })
+
+    const hotelEau = document.getElementById("hotelEau");
+    const hotelEauMoins = document.getElementById("hotelEauMoins");
+    const hotelEauPlus = document.getElementById("hotelEauPlus");
+    hotelEau.innerText=nbHotelEau;
+    cacher(nbHotelEau,hotelEauMoins)
+    hotelEauPlus.addEventListener("click",()=>{
+        nbHotelEau++;
+        hotelEau.innerText=nbHotelEau;
+        price+=(night*chambre);
+        finalPrice.innerText=price;
+        cacher(nbHotelEau,hotelEauMoins)
+    })
+    hotelEauMoins.addEventListener("click",()=>{
+        if(nbHotelEau>0){
+            nbHotelEau--;
+            hotelEau.innerText=nbHotelEau;
+            price-=(night*chambre);
+            finalPrice.innerText=price;
+            cacher(nbHotelEau,hotelEauMoins)
+        }
+    })
+
+    const hamacs = document.getElementById("hamacs");
+    const hamacsMoins = document.getElementById("hamacsMoins");
+    const hamacsPlus = document.getElementById("hamacsPlus");
+    hamacs.innerText=nbHamacs;
+    cacher(nbHamacs,hamacsMoins)
+    hamacsPlus.addEventListener("click",()=>{
+        nbHamacs++;
+        hamacs.innerText=nbHamacs;
+        price+=(night*aventure)
+        finalPrice.innerText=price;
+        cacher(nbHamacs,hamacsMoins)
+    })
+    hamacsMoins.addEventListener("click",()=>{
+        if(nbHamacs>0){
+            nbHamacs--;
+            hamacs.innerText=nbHamacs;
+            price-=(night*aventure)
+            finalPrice.innerText=price;
+            cacher(nbHamacs,hamacsMoins)
+        }
+    })
+
+    const chalet = document.getElementById("chalet");
+    const chaletMoins = document.getElementById("chaletMoins");
+    const chaletPlus = document.getElementById("chaletPlus");
+    chalet.innerText=nbChalet;
+    cacher(nbChalet,chaletMoins)
+    chaletPlus.addEventListener("click",()=>{
+        nbChalet++;
+        chalet.innerText=nbChalet;
+        price+=(night*prixChalet);
+        finalPrice.innerText=price;
+        cacher(nbChalet,chaletMoins)
+    })
+    chaletMoins.addEventListener("click",()=>{
+        if(nbChalet>0){
+            nbChalet--;
+            chalet.innerText=nbChalet;
+            price-=night*prixChalet;
+            finalPrice.innerText=price;
+            cacher(nbChalet,chaletMoins)
+        }
+    })
+        const camping = document.getElementById("camping");
+        const campingMoins = document.getElementById("campingMoins");
+        const campingPlus = document.getElementById("campingPlus");
+        camping.innerText=nbCamping;
+        cacher(nbCamping,campingMoins)
+        campingPlus.addEventListener("click",()=>{
+            nbCamping++;
+            camping.innerText=nbCamping;
+            price+=aventure*night
+            finalPrice.innerText=price;
+            cacher(nbCamping,campingMoins)
+        })
+        campingMoins.addEventListener("click",()=>{
+        if(nbCamping>0){
+            nbCamping--;
+            camping.innerText=nbCamping;
+            price-=aventure*night
+            finalPrice.innerText=price;
+            cacher(nbCamping,campingMoins)
+            }
+        })
+        const bulles = document.getElementById("bulles");
+        const bullesMoins = document.getElementById("bullesMoins");
+        const bullesPlus = document.getElementById("bullesPlus");
+        bulles.innerText=nbBulles;
+        cacher(nbBulles,bullesMoins)
+        bullesPlus.addEventListener("click",()=>{
+            nbBulles++;
+            bulles.innerText=nbBulles;
+            price+=chambre*night;
+            finalPrice.innerText=price;
+            cacher(nbBulles,bullesMoins)
+        })
+        bullesMoins.addEventListener("click",()=>{
+            if(nbBulles>0){
+                nbBulles--;
+                bulles.innerText=nbBulles;
+                price-=(chambre*night);
+                finalPrice.innerText=price;
+                cacher(nbBulles,bullesMoins)
+        }
+    })
+}
+
+priceNight();
+
