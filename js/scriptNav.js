902b2420c012e188a6f6d507ebb5e654c69015b6
Loïc <lolo62.grand@gmail.com>
2023-08-01 11:53:14 +0200

    :white_check_mark: page home avec les fonctionnalités dont celle à partir de 768px pour mon compte et les animations finis

--- js/scriptNav.js
+++ js/scriptNav.js
@@ -153,7 +153,7 @@ const gestion2 = document.createElement("li");
 const deco2 = document.createElement("li");
 // const backHome = document.createElement("a");
 gestion2.innerHTML=`<a>Modifier le profil</a>`;
-deco2.innerHTML=`<a href="home.html">Déconnexion</a>`;
+deco2.innerHTML=`<a href="index.html">Déconnexion</a>`;
 menu2.append(gestion2, deco2);
 btnCompte2.append(box2, menu2);
 
