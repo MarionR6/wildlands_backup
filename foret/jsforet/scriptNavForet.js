880e64b7d9da3774bbad9ad9c8db9c8c30dcf24a
MarionR6 <marionrenuy@gmail.com>
2023-08-02 16:18:05 +0200

    :bug: fixed - lien corrigé à la déconnexion

--- foret/jsforet/scriptNavForet.js
+++ foret/jsforet/scriptNavForet.js
@@ -64,7 +64,7 @@ const deco = document.createElement("li");
 // const backHome = document.createElement("a");
 gestion.innerHTML=`<a>Mon profil</a>`;
 panier.innerHTML=`<a>Mon panier</a>`;
-deco.innerHTML=`<a href="foret.html">Déconnexion</a>`;
+deco.innerHTML=`<a href="index.html">Déconnexion</a>`;
 menu.append(gestion, panier, deco);
 btnCompte.append(box, menu);
 
