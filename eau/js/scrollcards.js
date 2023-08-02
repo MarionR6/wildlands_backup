62daf53c1bd064f6872243a35db43f033a9e1f39
Pauline Todeschini <pauline.todeschini@gmail.com>
2023-08-02 09:35:43 +0200

    :sparkles:feat:ajout animations scrollcards & navScroll

---
+++ eau/js/scrollcards.js
@@ -0,0 +1,26 @@
+// REVEAL CARDS FROM LEFT AND RIGHT
+
+const ratio = .15;
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+
+const handleIntersect = (entries, observer) =>{
+    entries.forEach((entry) => {
+        if(entry.intersectionRatio > ratio){
+            entry.target.classList.add('revealvisible');
+        }else{
+            entry.target.classList.remove('revealvisible')
+        }
+    });
+}
+
+const observer = new IntersectionObserver (handleIntersect, options);
+document.querySelectorAll('.revealleft').forEach((r) => {
+    observer.observe(r)
+})
+document.querySelectorAll('.revealright').forEach((r) =>{
+    observer.observe(r)
+})
